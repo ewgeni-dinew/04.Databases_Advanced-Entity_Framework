9802d711672661c59cc2a41c8082b23e21ed960e
Evgeni Dinev <ewgeni.dinew@gmail.com>
2017-11-07 21:25:53 +0200

    --added task FULL solution

---
+++ 05.Introduction To Entity Framework/P02_DatabaseFirst/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using P02_DatabaseFirst.Data;
+using Microsoft.EntityFrameworkCore;
+using P02_DatabaseFirst.Data.Models;
+
+namespace P02_DatabaseFirst
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dbContext = new SoftUniContext();
+        }
+    }
+}
